f75caab1071b7f89167dcb6af63329a606afa2f4
Adi Prasetya (Atya) <88155210+adiprasetyaa@users.noreply.github.com>
2022-09-01 07:16:56 +0700

    menampilkan hello world

---
+++ 3rd-semester/praktikum-1/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class HelloWorld {
+    // main method
+    public static void main(String[] args) {
+        // menampilkan teks ke layar
+        System.out.println("halo, ini source code OOP Java buatanku");
+    }
+
+}
